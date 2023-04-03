c7bf0dee18f8e5ca3e5bb9f2190186b024780faa
Cocoa <cocoa@jiliguala.com>
2023-04-03 16:31:42 +0800

    broom 1.0.0

---
+++ broom.rb
@@ -0,0 +1,26 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Broom < Formula
+  desc ""
+  version "1.0.0"
+  head "https://github.com/jcder163/Broom.git"
+  url "https://github.com/jcder163/Broom/releases/download/#{version}/Broom-#{version}.zip"
+  
+  def install
+    bin.install 'Product/Broom'
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test Broom`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
