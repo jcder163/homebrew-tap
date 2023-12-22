b1c24468e7dc01f8141f84420cd95b5e4c6f3974
Cocoa <cocoa@jiliguala.com>
2023-12-22 15:20:12 +0800

    asc 1.0.1

---
+++ asc.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Asc < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/jcder163/ASC/releases/download/1.0.1/asc_1.0.1.tar.gz"
+  sha256 "2aa65df7ad851f9dbddbcc608dcf542a5595b22d40551e78c12046df7fc07075"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test ASC`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
